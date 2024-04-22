14442d4d564f49d68bf0d38dd32ccc0704c7139a
PaulK <paul.klarer@outlook.com>
2024-04-22 21:48:10 +0200

    Added more Model configuration Parmeters

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ def main():
     st.title("IBAIT GPT")
 
     client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-    model_options = ['gpt-3.5-turbo','gpt-4','gpt-4-turbo-2024-04-09']
-    
-    st.session_state["openai_model"] = st.selectbox('Choose your Model',model_options, index=0 )
-    model_temperature = st.slider('Temperature',0.0,2.0,0.7)
-
+    with st.sidebar:
+        st.title("IBAIT GPT Customizing")
+        model_options = ['gpt-3.5-turbo','gpt-4','gpt-4-turbo-2024-04-09']
+        st.session_state["openai_model"] = st.selectbox('Choose your Model',model_options, index=0 )
+        st.session_state["model_temperature"] = st.slider('Temperature',0.0,2.0,0.7)
+        st.session_state["frequency_penalty"] = st.slider('Frequncy Penalty',-2.0,2.0,0.0)
+        st.session_state["presence_penalty"] = st.slider('Presence Penalty',-2.0,2.0,0.0)
+        st.session_state["top_p"] = st.slider('Top_p Value', 0.0, 1.0, 0.9)
+        st.session_state["max_tokens"]=st.number_input("Max Tokens",0,16000,1000,250)
 
     if 'messages' not in st.session_state:
         st.session_state.messages = []
@@ -29,7 +33,11 @@ def main():
         model=st.session_state["openai_model"],
         messages= [{"role" : m["role"], "content": m["content"]}
                    for m in st.session_state.messages],
-        temperature= model_temperature,
+        temperature= st.session_state["model_temperature"],
+        frequency_penalty=st.session_state["frequency_penalty"],
+        top_p=st.session_state["top_p"],
+        presence_penalty=st.session_state["presence_penalty"],
+        max_tokens=st.session_state["max_tokens"],
         stream=True 
             ) 
             answer = st.write_stream(stream)
